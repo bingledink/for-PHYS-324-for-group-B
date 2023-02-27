156951bdf54b29c81ea9be2a906fbced1f48dc3f
bingledink <117133703+bingledink@users.noreply.github.com>
2023-02-27 16:55:48 -0500

    Add files via upload

--- scaler.py
+++ scaler.py
@@ -1,9 +1,12 @@
-with open('datalist.txt') as f:
-    lines = f.readlines()
+with open('datalist.txt') as f: #Opens file to be read
+    lines = f.readlines() #Puts lines of file into list
     f.close()
 
-with open('notnewdatalist', 'w') as j:
-    for i in lines:
-        j.write('python cutevents.py -i ' + i)
+with open('notnewdatalist.txt', 'w') as j: #Opens file to be written in
+    count = 0 
+    for i in lines: #Loops through list of lines, "i" is a specific line
+        string = 'python cutevents.py -i ' + i + "-o postcuts" + str(count) + ".root" #String to be written
+        j.write(string) #Writes string to file
+        count += 1
     
     j.close()
