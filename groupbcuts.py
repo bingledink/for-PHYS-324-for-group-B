452d2c8628356e303fc606ff36ead5ba144d861a
bingledink <117133703+bingledink@users.noreply.github.com>
2023-03-03 18:04:20 -0500

    Add files via upload

--- groupbcuts.py
+++ groupbcuts.py
@@ -11,8 +11,6 @@ args = parser.parse_args()
 
 fileptr = uproot.open(args.input)
 
-#Pull da data
-
 elec_pt = fileptr['Delphes_Ntuples']['elec_pt'].array()
 elec_eta = fileptr['Delphes_Ntuples']['elec_eta'].array()
 elec_charge = fileptr['Delphes_Ntuples']['elec_charge'].array()
@@ -32,10 +30,31 @@ jet_btag = fileptr['Delphes_Ntuples']['jet_btag'].array()
 jet_phi = fileptr['Delphes_Ntuples']['jet_phi'].array()
 jet_mass = fileptr['Delphes_Ntuples']['jet_phi'].array()
 
+#new
+met_pt = fileptr['Delphes_Ntuples']['met_pt'].array()
+met_phi = fileptr['Delphes_Ntuples']['met_phi'].array()
+
+weight = fileplr['Delphes_Ntuples']['weight'].array()
+scalar_ht = fileplr['Delphes_Ntuples']['scalar_ht'].array()
+
+genjet_pt = fileptr['Delphes_Ntuples']['genjet_pt'].array()
+genjet_eta = fileptr['Delphes_Ntuples']['genjet_eta'].array()
+genjet_phi = fileptr['Delphes_Ntuples']['genjet_phi'].array()
+genjet_mass = fileptr['Delphes_Ntuples']['genjet_mass'].array()
+
+genpart_pt = fileptr['Delphes_Ntuples']['genpart_pt'].array()
+genpart_eta = fileptr['Delphes_Ntuples']['genpart_eta'].array()
+genpart_phi = fileptr['Delphes_Ntuples']['genpart_phi'].array()
+genpart_mass = fileptr['Delphes_Ntuples']['genpart_mass'].array()
+genpart_pid = fileptr['Delphes_Ntuples']['genpart_pid'].array()
+genpart_status = fileptr['Delphes_Ntuples']['genpart_status'].array()
+genpart_charge = fileptr['Delphes_Ntuples']['genpart_charge'].array()
 
 
+#array for events that passed cuts
+onesnzeroes = 0 * len(elec_pt)
 
-#Setting arrays for tings
+
 e_pt = []
 e_eta = []
 e_phi = []
@@ -69,7 +88,7 @@ sl_eta = []
 sl_phi = []
 sl_mass = []
 
-#some function setting
+
 def deltaphi(e_phi, m_phi):
     d_phi = e_phi - m_phi
     if (d_phi > np.pi):
@@ -84,7 +103,6 @@ def dR(e_phi, e_eta, m_phi, m_eta):
     d_phi = deltaphi(e_phi, m_phi)
     return np.sqrt(d_phi**2 + d_eta**2)
 
-#Electron and muons
 for event_idx in range(len(elec_pt)):
     e_idx = []
     mu_idx = []
@@ -95,10 +113,7 @@ for event_idx in range(len(elec_pt)):
     counter = 0
     j_idx = []
     jf_idx = []
-
-    e4vector = ROOT.TLorentzVector()
-    mu4vector = ROOT.TLorentzVector()
-
+    
     for i in range(len(muon_pt[event_idx])):
         if muon_pt[event_idx][i] < 20:
             continue
@@ -135,7 +150,7 @@ for event_idx in range(len(elec_pt)):
 
     e_index = ef_idx[0]
     mu_index = muf_idx[0]
-#jets cuts
+
     for i in range(len(jet_pt[event_idx])):
         if jet_pt[event_idx][i] < 30:
             continue
@@ -161,7 +176,7 @@ for event_idx in range(len(elec_pt)):
     sljet_idx = j_idx[1]
 
 
-#Lepton cuts
+
     if elec_pt[event_idx][e_index] > muon_pt[event_idx][mu_index] and elec_pt[event_idx][e_index] > 25 :
         l_pt.append(elec_pt[event_idx][e_index])
         sl_pt.append(muon_pt[event_idx][mu_index])
@@ -189,7 +204,7 @@ for event_idx in range(len(elec_pt)):
 
 
 
-#append all the arrays
+
     e_pt.append(elec_pt[event_idx][e_index])
     e_eta.append(elec_eta[event_idx][e_index])
     e_phi.append(elec_phi[event_idx][e_index])
@@ -209,118 +224,13 @@ for event_idx in range(len(elec_pt)):
     sljet_phi.append(jet_phi[event_idx][sljet_idx])
     sljet_eta.append(jet_eta[event_idx][sljet_idx])
     sljet_mass.append(jet_mass[event_idx][sljet_idx])
+    
+    onesnzeroes[event_idx] = 1
 
 
-#print(len(l_pt))
-#print(counter)
+print(len(l_pt))
+print(counter)
 #print(j_pt)
-
-outputfile = ROOT.TFile("NewRoot.root", 'recreate')
-tree = ROOT.TTree("CutTree", "CutTree")
-
-# Electron arrays
-elec_pt_arr = array('f', [0.])
-elec_eta_arr = array('f', [0.])
-elec_phi_arr = array('f', [0.])
-elec_charge_arr = array('f', [0.])
-
-# Muon arrays
-muon_pt_arr = array('f', [0.])
-muon_eta_arr = array('f', [0.])
-muon_phi_arr = array('f', [0.])
-muon_charge_arr = array('f', [0.])
-
-# Jet arrays
-ljet_pt_arr = array('f', [.0])
-ljet_eta_arr = array('f', [.0])
-ljet_phi_arr = array('f', [.0])
-ljet_mass_arr = array('f', [.0])
-
-sljet_pt_arr = array('f', [.0])
-sljet_eta_arr = array('f', [.0])
-sljet_phi_arr = array('f', [.0])
-sljet_mass_arr = array('f', [.0])
-# Lepton arrays
-l_pt_arr = array('f', [.0])
-l_eta_arr = array('f', [.0])
-l_phi_arr = array('f', [.0])
-l_mass_arr = array('f', [.0])
-
-sl_pt_arr = array('f', [.0])
-sl_eta_arr = array('f', [.0])
-sl_phi_arr = array('f', [.0])
-sl_mass_arr = array('f', [.0])
-
-# Elec branches
-tree.Branch("elec_pt", elec_pt_arr, "elec_pt/F")
-tree.Branch("elec_eta", elec_eta_arr, "elec_eta/F")
-tree.Branch("elec_phi", elec_phi_arr, "elec_phi/F")
-tree.Branch("elec_charge", elec_charge_arr, "elec_charge/F")
-
-# Muon branches
-tree.Branch("muon_pt", muon_pt_arr, "muon_pt/F")
-tree.Branch("muon_eta", muon_eta_arr, "muon_eta/F")
-tree.Branch("muon_phi", muon_phi_arr, "muon_phi/F")
-tree.Branch("muon_charge", muon_charge_arr, "muon_charge/F")
-
-# Jets branches
-tree.Branch("ljet_pt", ljet_pt_arr, "ljet_pt/F")
-tree.Branch("ljet_eta", ljet_eta_arr, "ljet_eta/F")
-tree.Branch("ljet_phi", ljet_phi_arr, "ljet_phi/F")
-tree.Branch("ljet_mass", ljet_mass_arr, "ljet_mass/F")
-tree.Branch("sljet_pt", sljet_pt_arr, "sljet_pt/F")
-tree.Branch("sljet_eta", sljet_eta_arr, "sljet_eta/F")
-tree.Branch("sljet_phi", sljet_phi_arr, "sljet_phi/F")
-tree.Branch("sljet_mass", sljet_mass_arr, "sljet_mass/F")
-
-# Lepton Branches
-tree.Branch("l_pt", l_pt_arr, "l_pt/F")
-tree.Branch("l_eta", l_eta_arr, "l_eta/F")
-tree.Branch("l_phi", l_phi_arr, "l_phi/F")
-tree.Branch("l_mass", l_mass_arr, "l_mass/F")
-tree.Branch("sl_pt", sl_pt_arr, "sl_pt/F")
-tree.Branch("sl_eta", sl_eta_arr, "sl_eta/F")
-tree.Branch("sl_phi", sl_phi_arr, "sl_phi/F")
-tree.Branch("sl_mass", sl_mass_arr, "sl_mass/F")
-
-# originaltags = [mu_pt, mu_eta, mu_phi, mu_charge]
-# flattags = [mu_pt_flat, mu_eta_flat, mu_phi_flat, mu_charge_flat]
-
-for i in range(len(e_pt)):
-    elec_pt_arr[0] = e_pt[i]
-    elec_phi_arr[0] = e_phi[i]
-    elec_eta_arr[0] = e_eta[i]
-    elec_charge_arr[0] = e_charge[i]
-
-    muon_pt_arr[0] = mu_pt[i]
-    muon_eta_arr[0] = mu_eta[i]
-    muon_phi_arr[0] = mu_phi[i]
-    muon_charge_arr[0] = mu_charge[i]
-
-    ljet_pt_arr[0] = ljet_pt[i]
-    ljet_eta_arr[0] = ljet_eta[i]
-    ljet_phi_arr[0] = ljet_phi[i]
-    ljet_mass_arr[0] = ljet_mass[i]
-
-    sljet_pt_arr[0] = sljet_pt[i]
-    sljet_eta_arr[0] = sljet_eta[i]
-    sljet_phi_arr[0] = sljet_phi[i]
-    sljet_mass_arr[0] = sljet_mass[i]
-
-    l_pt_arr[0] = l_pt[i]
-    l_eta_arr[0] = l_eta[i]
-    l_phi_arr[0] = l_phi[i]
-    l_mass_arr[0] = l_mass[i]
-
-    sl_pt_arr[0] = sl_pt[i]
-    sl_eta_arr[0] = sl_eta[i]
-    sl_phi_arr[0] = sl_phi[i]
-    sl_pt_arr[0] = sl_mass[i]
-
-    tree.Fill()
-
-outputfile.Write()
-outputfile.Close()
 
 
 
